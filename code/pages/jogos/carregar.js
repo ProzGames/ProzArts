4132124b73ad7571bd484909326891b854525694
Vanderson <vanderson.santos94@hotmail.com>
2024-04-22 19:26:29 -0300

    demais alterações na pagina de JOGOS

--- code/pages/jogos/carregar.js
+++ code/pages/jogos/carregar.js
@@ -2,5 +2,6 @@ const verMaisJogos = document.getElementById('verMaisJogos');
 const maisJogosDiv = document.getElementById('maisJogos');
 
 verMaisJogos.addEventListener('click', () => {    
-    maisJogosDiv.classList.toggle('hidden');
+    maisJogosDiv.classList.toggle('hidden');    
+    verMaisJogos.classList.toggle('hidden');
 });
