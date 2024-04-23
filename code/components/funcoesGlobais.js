90893b50b0b64ca2a60346d17036034c3d8d7cf7
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-04-23 19:13:43 -0300

    adicionando estilo dinamico para formulario

--- code/components/funcoesGlobais.js
+++ code/components/funcoesGlobais.js
@@ -4,7 +4,7 @@
  * Usado para retornar a URL atual
  * @returns String
  */
-function urlLocal(){
+function urlLocal() {
   return window.location.href
 }
 
@@ -12,21 +12,21 @@ function urlLocal(){
  * Usado para retornar o URI local
  * @returns String
  */
-function uriLocal(){
+function uriLocal() {
   const urlLocal = window.location.href
 
   let array_caminho = urlLocal.split("/")
 
   //procurar a raiz do site
   let i = 0
-  for(i = 0 ; i < array_caminho.length ; i++){
-    if(array_caminho[i] === "code") break;
+  for (i = 0; i < array_caminho.length; i++) {
+    if (array_caminho[i] === "code") break;
   }
 
   //concatenar o restante do vetor
-  i += 1 ; let str = ""
-  for(j = i ; j < array_caminho.length ; j++){
-    if(j < array_caminho.length && j !== i){
+  i += 1; let str = ""
+  for (j = i; j < array_caminho.length; j++) {
+    if (j < array_caminho.length && j !== i) {
       str += "/"
     }
 
@@ -40,7 +40,7 @@ function uriLocal(){
  * Usado para retornar a profundidade do arquivo index.html com relação a raiz do projeto (Site)
  * @returns Inteiro
  */
-function profundidade(){
+function profundidade() {
   return uriLocal().split("/").length - 1
 }
 
@@ -48,13 +48,16 @@ function profundidade(){
  * Usado para retornar a localização do diretorio raiz do projeto html
  * @returns String
  */
-function localizacao_da_raiz(){
+function localizacao_da_raiz() {
   const p = profundidade()
-  if(p === 0) return "./"
+  if (p === 0) return "./"
 
   let str = ""
-  for(let i = 0 ; i < p ; i++)
+  for (let i = 0; i < p; i++)
     str += "../"
 
   return str
 }
+
+//------------------------------------------------------------------------------------------------
+//VALIDAÇÃO DE CAMPOS
