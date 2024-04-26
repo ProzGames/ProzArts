c43beeb4a67842e84e52eca54bd1b0909334da52
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-04-26 09:57:03 -0300

    finalizando exemplo de validação de campo de email e inserindo identificadores para as mensagens de erro dos respectivos campos de formulário

--- code/pages/cadastro/script.js
+++ code/pages/cadastro/script.js
@@ -1,4 +1,5 @@
 const input_email = document.getElementById("email")
+const msg_erro_email = document.getElementById("error-email")
 const form = document.querySelector("form")
 
 //status de todos os campos
@@ -14,6 +15,16 @@ input_email.addEventListener("change", () => {
   console.log(validacao_input_email(input_email.value))
   if (validacao_input_email(input_email.value)) {
     status_input.email = true;
+
+    if (input_email.classList.contains("campo-errado")) {
+      input_email.classList.remove("campo-errado");
+      msg_erro_email.style.display = "none";
+    }
+  } else {
+    if (!input_email.classList.contains("campo-errado")) {
+      input_email.classList.add("campo-errado");
+      msg_erro_email.style.display = "block";
+    }
   }
 })
 
