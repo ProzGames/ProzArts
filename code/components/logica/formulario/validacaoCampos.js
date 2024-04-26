70cf4297fee88a6c8761bac30f9598a2d8e01332
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-04-26 18:42:38 -0300

    fazendo a validação do campo de repetir email e adicionando o verificador de data correta

--- code/components/logica/formulario/validacaoCampos.js
+++ code/components/logica/formulario/validacaoCampos.js
@@ -1,7 +1,24 @@
+/**
+ * 
+ * @param {string} e 
+ * @returns {boolean}
+ */
 function validacao_input_email(e) {
   //RegExp é um objeto para declarar expressões regulares em JS
   const exprRegular =
     new RegExp(/^[a-zA-Z0-9][a-zA-Z0-9._]+@((gmail)|(hotmail)|(outlook)).com/, 'g');
 
   return exprRegular.test(e);
+}
+
+function validacao_data(e) {
+  const data = e.split("-")
+  const dia = data[2]; const mes = data[1]; const ano = data[0]
+
+  const dia_correto = dia > 0 && dia < 32;
+  const mes_correto = mes > 0 && mes < 13;
+  const ano_correto = ano > 1900 && ano < (new Date(Date.now()).getFullYear() - 6)
+
+  console.log(dia_correto + " " + mes_correto + " " + ano_correto)
+  return dia_correto && mes_correto && ano_correto;
 }
--- code/pages/cadastro/script.js
+++ code/pages/cadastro/script.js
@@ -1,15 +1,27 @@
 const input_email = document.getElementById("email")
+const input_confirmar_email = document.getElementById("confirmar_email")
+const input_dt_nasc = document.getElementById("data_nascimento")
 const msg_erro_email = document.getElementById("error-email")
+const msg_erro_confirmar_email = document.getElementById("error-confirmar-email")
 const form = document.querySelector("form")
 const btn_submit = document.getElementById("enviar_forms")
 
 //status de todos os campos
 const status_input = {
-  nome: false,
+  nome: true,
   email: false,
-  dt_nascimento: false,
-  senha: false,
-  termo_uso_e_privacidade: false
+  email_confirmado: false,
+  dt_nascimento: true,
+  senha: true,
+  termo_uso_e_privacidade: true
+}
+
+function verificar_campos_preenchidos() {
+  if (!(status_input.dt_nascimento && status_input.email && status_input.nome && status_input.senha && status_input.termo_uso_e_privacidade && status_input.email_confirmado)) {
+    btn_submit.disabled = "true"
+  } else {
+    btn_submit.removeAttribute("disabled")
+  }
 }
 
 input_email.addEventListener("change", () => {
@@ -20,28 +32,49 @@ input_email.addEventListener("change", () => {
       input_email.classList.remove("campo-errado");
       msg_erro_email.style.display = "none";
     }
-
-    btn_submit.removeAttribute("disabled")
   } else {
     status_input.email = false;
-
     if (!input_email.classList.contains("campo-errado")) {
       input_email.classList.add("campo-errado");
       msg_erro_email.style.display = "block";
     }
-    btn_submit.disabled = "true"
   }
+
+  verificar_campos_preenchidos()
 })
+
+input_confirmar_email.addEventListener("change", () => {
+  if (validacao_input_email(input_confirmar_email.value)) {
+    if (input_confirmar_email.value === input_email.value) {
+      status_input.email_confirmado = true;
+
+      if (input_confirmar_email.classList.contains("campo-errado")) {
+        input_confirmar_email.classList.remove("campo-errado");
+        msg_erro_confirmar_email.style.display = "none";
+      }
+    } else {
+      status_input.email_confirmado = false;
+
+      if (!input_confirmar_email.classList.contains("campo-errado")) {
+        input_confirmar_email.classList.add("campo-errado");
+        msg_erro_confirmar_email.style.display = "block";
+      }
+    }
+  } else {
+    status_input.email_confirmado = false;
+
+    if (!input_confirmar_email.classList.contains("campo-errado")) {
+      input_confirmar_email.classList.add("campo-errado");
+      msg_erro_confirmar_email.style.display = "block";
+    }
+  }
+  verificar_campos_preenchidos()
+})
+
 
 form.addEventListener("submit", (e) => {
   e.preventDefault()
 
-  /*
-  if (!(status_input.dt_nascimento && status_input.email && status_input.nome && status_input.senha && status_input.termo_uso_e_privacidade)) {
-    //tem algum campo invalido
-    alert("Preencha o campo corretamente");
-    btn_submit.disabled = "true"
-  }*/
 
   cadastro_usuario("", input_email.value, new Date(2001, 11, 16), "", false);
   console.log(JSON.parse(sessionStorage.getItem("usuarios")))
