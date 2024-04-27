1c99f7a58bb269749caf14ecd61e961acf56e38d
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-04-27 19:35:02 -0300

    fazendo a validação de termos de uso

--- code/pages/cadastro/script.js
+++ code/pages/cadastro/script.js
@@ -1,9 +1,18 @@
+//inputs
+const input_nome_usuario = document.getElementById("nome_usuario")
 const input_email = document.getElementById("email")
 const input_confirmar_email = document.getElementById("confirmar_email")
 const input_dt_nasc = document.getElementById("data_nascimento")
+const input_Senha = document.getElementById("senha")
+const input_repete_Senha = document.getElementById("repete_senha")
+const input_termo_de_uso = document.getElementById("termos_check")
+const input_aceitar_conteudos_extras = document.getElementById("conteudo_check")
+//msg de erro
 const msg_erro_email = document.getElementById("error-email")
 const msg_erro_confirmar_email = document.getElementById("error-confirmar-email")
 const msg_erro_dt_nascimento = document.getElementById("error-dt-nascimento")
+const msg_erro_termo_de_uso = document.getElementById("error-termo-de-uso");
+//formulario
 const form = document.querySelector("form")
 const btn_submit = document.getElementById("enviar_forms")
 
@@ -13,12 +22,25 @@ const status_input = {
   email: false,
   email_confirmado: false,
   dt_nascimento: false,
-  senha: true,
-  termo_uso_e_privacidade: true
+  senha: true
 }
 
+/**
+ * @returns(boolean) se todos os campos foram preenchidos corretamente
+ */
 function verificar_campos_preenchidos() {
-  if (!(status_input.dt_nascimento && status_input.email && status_input.nome && status_input.senha && status_input.termo_uso_e_privacidade && status_input.email_confirmado)) {
+  if (!(status_input.dt_nascimento && status_input.email && status_input.nome && status_input.senha && status_input.email_confirmado)) {
+    return false
+  } else {
+    return true
+  }
+}
+
+/**
+ * Altera o status do botão de enviar formulario
+ */
+function validar_ativacao_do_botao() {
+  if (!verificar_campos_preenchidos()) {
     btn_submit.disabled = "true"
   } else {
     btn_submit.removeAttribute("disabled")
@@ -41,7 +63,7 @@ input_email.addEventListener("change", () => {
     }
   }
 
-  verificar_campos_preenchidos()
+  validar_ativacao_do_botao()
 })
 
 input_confirmar_email.addEventListener("change", () => {
@@ -69,10 +91,9 @@ input_confirmar_email.addEventListener("change", () => {
       msg_erro_confirmar_email.style.display = "block";
     }
   }
-  verificar_campos_preenchidos()
+  validar_ativacao_do_botao()
 })
 
-console.log(input_dt_nasc)
 input_dt_nasc.addEventListener("change", () => {
   if (validacao_data(input_dt_nasc.value)) {
     if (input_dt_nasc.classList.contains("campo-errado")) {
@@ -87,17 +108,20 @@ input_dt_nasc.addEventListener("change", () => {
     }
     status_input.dt_nascimento = false
   }
-  verificar_campos_preenchidos()
+  validar_ativacao_do_botao()
 })
 
 form.addEventListener("submit", (e) => {
   e.preventDefault()
 
-
-  cadastro_usuario("", input_email.value, new Date(2001, 11, 16), "", false);
-  console.log(JSON.parse(sessionStorage.getItem("usuarios")))
-  setTimeout(() => {
-    alert("Usuário cadastrado com sucesso.");
-    window.location.href = localizacao_da_raiz() + "index.html"
-  }, 5000);
+  if (input_termo_de_uso.checked) { //se o usuario aceitar os termos de uso, ele irá cadastrar
+    cadastro_usuario(input_nome_usuario.value, input_email.value, new Date(2001, 11, 16), input_Senha.value, input_aceitar_conteudos_extras.checked);
+    console.log(JSON.parse(sessionStorage.getItem("usuarios")))
+    setTimeout(() => {
+      alert("Usuário cadastrado com sucesso.");
+      window.location.href = localizacao_da_raiz() + "index.html"
+    }, 5000);
+  } else {
+    msg_erro_termo_de_uso.style.display = "block"
+  }
 })
